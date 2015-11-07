1d5e24e424a41b2d062dcecb5e248d4b786cba12
Oskari Lehto <oskari.lehto@gmail.com>
2015-11-07 18:52:12 +0200

    fix whitespaces

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,27 @@
 var app = angular.module('myApp', ['ngRoute']);
 
 app.config(function($routeProvider) {
-    $routeProvider
-    .when('/', {
-        templateUrl : 'static/login.html',
-        controller  : 'LoginController'
-    })
-    .when('/login', {
-        templateUrl : 'static/login.html',
-        controller  : 'LoginController'
-    })
-    .when('/flights', {
-        templateUrl : 'static/flights.html',
-        controller  : 'AllOrdersController',
-        controllerAs : 'allorders'
-    })
-    .when('/orders', {
-        templateUrl : 'static/orders.html',
-        controller  : 'UserOrdersController',
-        controllerAs : 'orders'
-    })
-    .when('/createorder', {
-        templateUrl : 'static/createorder.html',
-        controller  : 'CreateOrderController'
-    })
-
-;})
+  $routeProvider
+  .when('/', {
+    templateUrl: 'static/login.html',
+    controller: 'LoginController'
+  })
+  .when('/login', {
+    templateUrl: 'static/login.html',
+    controller: 'LoginController'
+  })
+  .when('/flights', {
+    templateUrl: 'static/flights.html',
+    controller: 'AllOrdersController',
+    controllerAs: 'allorders'
+  })
+  .when('/orders', {
+    templateUrl: 'static/orders.html',
+    controller: 'UserOrdersController',
+    controllerAs: 'orders'
+  })
+  .when('/createorder', {
+    templateUrl: 'static/createorder.html',
+    controller: 'CreateOrderController'
+  })
+});
