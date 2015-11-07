e353243c65098e69a58141d2e52aa321aa853046
oukka <annika@giosg.com>
2015-11-07 16:40:11 +0200

    Some mock data

--- js/all_orders.controller.js
+++ js/all_orders.controller.js
@@ -1,15 +1,21 @@
 app.controller('AllOrdersController', function($scope) {
+    this.flight = [
+        {'date': '2015-11-07', 'time': '16-15-00', 'year': '2015', 'airport': 'YXU', 'destination': 'MIO'},
+        {'date': '2015-11-07', 'time': '16-15-00', 'year': '2015', 'airport': 'YXU', 'destination': 'OBT'},
+        {'date': '2015-11-08', 'time': '16-15-00', 'year': '2015', 'airport': 'YXU', 'destination': 'AMS'},
+        {'date': '2015-11-08', 'time': '16-15-00', 'year': '2015', 'airport': 'YXU', 'destination': 'JSS'},
+        {'date': '2015-11-08', 'time': '16-15-00', 'year': '2015', 'airport': 'YXU', 'destination': 'BWU'}
+    ];
 
     /*
-
-	Show all data from all flights based on user location
+    Show all data from all flights based on user location
 
     Flights:
     - departure date
     - departure time
     - departure year
-	- departure air port
-	- destination
+    - departure air port
+    - destination
 
     */
 
