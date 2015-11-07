a388159a04758113ca5403a40def4414e7b252e8
Oskari Lehto <oskari.lehto@gmail.com>
2015-11-07 18:41:19 +0200

    jou

--- application.py
+++ application.py
@@ -20,12 +20,14 @@ class Application(Flask):
         assets = Environment(self)
         assets.load_path = [
             os.path.join(os.path.dirname(__file__), 'bower_components'),
+            os.path.join(os.path.dirname(__file__), 'static/js'),
             # os.path.join(os.path.dirname(__file__), 'bower_components'),
         ]
         assets.register(
             'js_all',
             Bundle(
-                '**/**.js',
+                '**/**.min.js',
+                'js/**.min.js',
                 output='js_all.js'
             )
         )
---
+++ static/js/controllers/all_orders.controller.js
@@ -0,0 +1,24 @@
+app.controller('AllOrdersController', function($scope) {
+    this.flights = [
+        {'date': '2015-11-07', 'time': '16-15-00', 'year': '2015', 'airport': 'London', 'destination': 'Miami'},
+        {'date': '2015-11-07', 'time': '16-15-00', 'year': '2015', 'airport': 'London', 'destination': 'Oakland'},
+        {'date': '2015-11-08', 'time': '16-15-00', 'year': '2015', 'airport': 'London', 'destination': 'Amsterdam'},
+        {'date': '2015-11-08', 'time': '16-15-00', 'year': '2015', 'airport': 'London', 'destination': 'Lissabon'},
+        {'date': '2015-11-08', 'time': '16-15-00', 'year': '2015', 'airport': 'London', 'destination': 'Sydney'}
+    ];
+
+    /*
+    Show all data from all flights based on user location
+
+    Flights:
+    - departure date
+    - departure time
+    - departure year
+    - departure air port
+    - destination
+
+    */
+
+
+
+});
