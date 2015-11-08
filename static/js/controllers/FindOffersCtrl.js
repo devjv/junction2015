dca6516e7bfba9b0712c7e930efa688e9af92874
oukka <annika@giosg.com>
2015-11-08 12:49:01 +0200

    changes

--- static/js/controllers/FindOffersCtrl.js
+++ static/js/controllers/FindOffersCtrl.js
@@ -1,6 +1,5 @@
 app.controller('FindOffersCtrl', ['$scope', '$rootScope', 'ParamsService',
     function($scope, $rootScope, ParamsService) {
-    self = this;
     $scope.flight;
 
     $scope.flightChanged = function() {
@@ -13,16 +12,16 @@ app.controller('FindOffersCtrl', ['$scope', '$rootScope', 'ParamsService',
         self.offers = [];
 
         $rootScope.currentPage = 'find';
-        console.log($scope.flight);
-        ParamsService.getOffersForFlight($scope.flight).then(
-            function(respose) {
-                console.log(respose);
-                self.offers = respose.data.result.map(function(flight) {
-                    flight.offered_flight.departure_time = new Date(flight.offered_flight.departure_time);
-                    return flight;
-                });
-                console.log(self.ordered_flights);
+        console.log($scope);
+        // ParamsService.getOffersForFlight($scope.flight).then(
+        //     function(respose) {
+        //         console.log(respose);
+        //         self.offers = respose.data.result.map(function(flight) {
+        //             flight.offered_flight.departure_time = new Date(flight.offered_flight.departure_time);
+        //             return flight;
+        //         });
+        //         console.log(self.ordered_flights);
 
-            }
-        );
+        //     }
+        // );
 }]);
