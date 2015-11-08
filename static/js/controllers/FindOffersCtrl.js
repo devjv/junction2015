b8f888eef82adcd72d7911f81089f3b82568695a
Jere Vaara <jerevaara@gmail.com>
2015-11-08 12:43:58 +0200

    oukka pls

--- static/js/controllers/FindOffersCtrl.js
+++ static/js/controllers/FindOffersCtrl.js
@@ -1,28 +1,25 @@
 app.controller('FindOffersCtrl', ['$scope', '$rootScope', 'ParamsService',
     function($scope, $rootScope, ParamsService) {
-    self = this;
     $scope.flight;
 
     $scope.flightChanged = function() {
+        console.log("flightChanged event");
         console.log($scope.flight);
     };
 
 
     $rootScope.currentPage = 'find';
+    console.log($scope.flight);
+    /*ParamsService.getOffersForFlight($scope.flight).then(
+        function(respose) {
+            console.log(respose);
+            self.offers = respose.data.result.map(function(flight) {
+                flight.offered_flight.departure_time = new Date(flight.offered_flight.departure_time);
+                return flight;
+            });
+            console.log(self.ordered_flights);
 
-        self.offers = [];
+        }
+    );*/
 
-        $rootScope.currentPage = 'find';
-        console.log($scope.flight);
-        ParamsService.getOffersForFlight($scope.flight).then(
-            function(respose) {
-                console.log(respose);
-                self.offers = respose.data.result.map(function(flight) {
-                    flight.offered_flight.departure_time = new Date(flight.offered_flight.departure_time);
-                    return flight;
-                });
-                console.log(self.ordered_flights);
-
-            }
-        );
 }]);
